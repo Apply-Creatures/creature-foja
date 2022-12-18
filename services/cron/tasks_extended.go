c53f802778c1951e0804507eec995bca37f1b09b
Gusted <postmaster@gusted.xyz>
2022-12-18 19:56:08 +0100

    [PRIVACY] Disable `update_checker` cron tasks
    - This is being disabled as it will weekly connect to a domain.
    - This only affects existing installations, as new installations will
    have a explicit value being written into app.ini due to https://github.com/go-gitea/gitea/pull/21655
    
    (cherry picked from commit cd0b8b6852563118ab8530c01a48bc612efcf58a)
    (cherry picked from commit 58d43867cab15a4b93fab6f4b467756e8030fb2d)
    (cherry picked from commit f290b91e504933be83d5b9540a195173280ab38d)
    (cherry picked from commit 5bd6d7555478713c91a3c00980060041ed34ba22)
    (cherry picked from commit e5d0abb59244809f5181dfbadfbee47531246a18)
    (cherry picked from commit 4b381ce77e699ce9f0f8af8ac3488bd8e3562b15)
    (cherry picked from commit b6a9b1390d9ee3aa70d50b1fae1346aff69cab08)
    (cherry picked from commit 367436287118b0fc4030ee6527d6c366db919594)
    (cherry picked from commit af0f606fc1f42be49dcbedd04055f0f9a0b7d3df)
    (cherry picked from commit 49560da6fb01cd6ee1a69840f48ed9269330c37a)
    (cherry picked from commit ca06e27c5b7b8b20e34935885c4ab0ca905b9157)
    (cherry picked from commit 163df86df5abdda11b6d337af32e0f0f3837119b)
    (cherry picked from commit 0391c6ae42eb364a6db1156af93304aa73c69b2d)
    (cherry picked from commit f282f2d42fd850c29b489d02133698e14b21f411)
    (cherry picked from commit 68d31cf2478626730324eaa56ad4264580b7030c)
    (cherry picked from commit cfd4a479e95597f315d9d75e2b7d22c8f6941ce2)
    (cherry picked from commit add969bdf624df9eed3047d60a1bae979e9bfd80)
    (cherry picked from commit c0f3f1f1e733b6df0f028a1393549adf7f8936f4)
    (cherry picked from commit f11dca9090a77dbd58ff980e97cff07a5b2d27b5)
    (cherry picked from commit 283810387ff26fbbf1ac795d56350a7bfb87b874)
    (cherry picked from commit 08af1f0ea882bbe3fada3e61988baa9f0d290d1a)
    (cherry picked from commit 1c1a6da319f9218cc05693e0b8461f3defe1d891)
    (cherry picked from commit c1444bae65daa29497209a0bcc0758b1af9828a0)
    (cherry picked from commit 6361df634bd8fda81793e35820f0a42c56e5e55a)
    (cherry picked from commit cc8bb3f94605858c3ff9412644d90d80150c3f23)
    (cherry picked from commit c7ae4411fd041dde1c73a5fdc64679317b6daef7)
    (cherry picked from commit f73f4eef02c41430cc6a7375fc3b73aab8b75de7)
    (cherry picked from commit 2122896e8529db6b6158bb9029492f8f5e12efb7)
    (cherry picked from commit d469c99671cf6a5a8087b1ad106a04b43f4edc85)
    (cherry picked from commit 41dbbd731edf52c1c78d34cba1dfd7e03029e5f7)
    (cherry picked from commit e8e742b4376ab2437097a94a5f541576f9da3c81)
    (cherry picked from commit 0fa9f87b5fbb81b9205650da918bab02c7f86916)
    (cherry picked from commit a92c724a563e0260425cfe8569dec893740b8c71)
    (cherry picked from commit 6a18d153e1f60bdac04a568eaa2d74cb2c1d5cb1)
    (cherry picked from commit 0dd35a13fdcf34f88b9cc554646402b1bb40da57)
    (cherry picked from commit 5ae0cbe0ae98c7e12666f185aa5da21160ab83e8)
    (cherry picked from commit 13bd3cf37bed863491f1284572fa0bfa470b8ec0)
    (cherry picked from commit a163efa6cf5746a2ac3cfec78ff8c5d70158e3f2)
    (cherry picked from commit 422a6e8329abf9a961222a50ce136af84f4f2975)
    (cherry picked from commit 125fb33e1baad8bf88d71db5bffaa898c38565e9)
    (cherry picked from commit fd39d15a921c726f593a55ba0c4eae6f8f78a4a0)
    (cherry picked from commit b58147451372d354a499f6b38d2662ff6c74c132)
    (cherry picked from commit 987d19852364fe258b4eaddd9df51245a082522e)
    (cherry picked from commit f06eed025fb73ca1ebe7eb65cbd5ab5c3276d981)
    (cherry picked from commit 6f9f4156448e7b9eb9d7c7776eb20b0ab5de692c)
    (cherry picked from commit eb2c878debf07ad46e272ff8e2adfa414657f4c3)
    (cherry picked from commit 70daffa9fe3a4007d199ea346f0d14f37917ac5b)
    (cherry picked from commit 3a6ae6fa49d238bed04c1fd1342b054dab7b6f58)
    (cherry picked from commit 682a564b04a62aeea15e08a833ae2b158d1bfe67)

--- services/cron/tasks_extended.go
+++ services/cron/tasks_extended.go
@@ -146,7 +146,7 @@ func registerUpdateGiteaChecker() {
 	}
 	RegisterTaskFatal("update_checker", &UpdateCheckerConfig{
 		BaseConfig: BaseConfig{
-			Enabled:    true,
+			Enabled:    false,
 			RunAtStart: false,
 			Schedule:   "@every 168h",
 		},
