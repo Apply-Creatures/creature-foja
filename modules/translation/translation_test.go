075e0de142d450f7e6083f56b153e6eacbd5a4a1
0ko <0ko@noreply.codeberg.org>
2024-03-19 18:47:20 +0500

    Add unit test for TrSize

--- modules/translation/translation_test.go
+++ modules/translation/translation_test.go
@@ -3,6 +3,8 @@
 
 package translation
 
+// TODO: make this package friendly to testing
+
 import (
 	"testing"
 
@@ -11,9 +13,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrettyNumber(t *testing.T) {
-	// TODO: make this package friendly to testing
+func TestTrSize(t *testing.T) {
+	l := NewLocale("")
+	size := int64(1)
+	assert.EqualValues(t, "1 munits.data.b", l.TrSize(size).String())
+	size *= 2048
+	assert.EqualValues(t, "2 munits.data.kib", l.TrSize(size).String())
+	size *= 2048
+	assert.EqualValues(t, "4 munits.data.mib", l.TrSize(size).String())
+	size *= 2048
+	assert.EqualValues(t, "8 munits.data.gib", l.TrSize(size).String())
+	size *= 2048
+	assert.EqualValues(t, "16 munits.data.tib", l.TrSize(size).String())
+	size *= 2048
+	assert.EqualValues(t, "32 munits.data.pib", l.TrSize(size).String())
+	size *= 128
+	assert.EqualValues(t, "4 munits.data.eib", l.TrSize(size).String())
+}
 
+func TestPrettyNumber(t *testing.T) {
 	i18n.ResetDefaultLocales()
 
 	allLangMap = make(map[string]*LangType)
