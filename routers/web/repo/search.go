488a99fb56be8c752efa797a45a80a659272754c
Shiny Nematoda <snematoda.751k2@aleeas.com>
2024-03-28 05:09:41 +0000

    Reverts commits releated to git-grep
    This reverts commit
    	51fb6f3983f15aa3c2db1feadcc13db1692315ec
    	71a72d8a22f2ba5d08415542371f3529a01571dd

--- routers/web/repo/search.go
+++ routers/web/repo/search.go
@@ -11,13 +11,17 @@ import (
 	code_indexer "code.gitea.io/gitea/modules/indexer/code"
 	"code.gitea.io/gitea/modules/setting"
 	"code.gitea.io/gitea/services/context"
-	"code.gitea.io/gitea/services/repository/files"
 )
 
 const tplSearch base.TplName = "repo/search"
 
 // Search render repository search page
 func Search(ctx *context.Context) {
+	if !setting.Indexer.RepoIndexerEnabled {
+		ctx.Redirect(ctx.Repo.RepoLink)
+		return
+	}
+
 	language := ctx.FormTrim("l")
 	keyword := ctx.FormTrim("q")
 
@@ -33,57 +37,32 @@ func Search(ctx *context.Context) {
 		return
 	}
 
-	ctx.Data["Repo"] = ctx.Repo.Repository
-
 	page := ctx.FormInt("page")
 	if page <= 0 {
 		page = 1
 	}
-
-	if setting.Indexer.RepoIndexerEnabled {
-		ctx.Data["CodeIndexerDisabled"] = false
-
-		total, searchResults, searchResultLanguages, err := code_indexer.PerformSearch(ctx, &code_indexer.SearchOptions{
-			RepoIDs:        []int64{ctx.Repo.Repository.ID},
-			Keyword:        keyword,
-			IsKeywordFuzzy: isFuzzy,
-			Language:       language,
-			Paginator: &db.ListOptions{
-				Page:     page,
-				PageSize: setting.UI.RepoSearchPagingNum,
-			},
-		})
-		if err != nil {
-			if code_indexer.IsAvailable(ctx) {
-				ctx.ServerError("SearchResults", err)
-				return
-			}
-			ctx.Data["CodeIndexerUnavailable"] = true
-		} else {
-			ctx.Data["CodeIndexerUnavailable"] = !code_indexer.IsAvailable(ctx)
-		}
-
-		ctx.Data["SearchResults"] = searchResults
-		ctx.Data["SearchResultLanguages"] = searchResultLanguages
-
-		pager := context.NewPagination(total, setting.UI.RepoSearchPagingNum, page, 5)
-		pager.SetDefaultParams(ctx)
-		pager.AddParam(ctx, "l", "Language")
-		ctx.Data["Page"] = pager
-	} else {
-		data, err := files.NewRepoGrep(ctx, ctx.Repo.Repository, keyword)
-		if err != nil {
-			ctx.ServerError("NewRepoGrep", err)
+	
+	total, searchResults, searchResultLanguages, err := code_indexer.PerformSearch(ctx, []int64{ctx.Repo.Repository.ID},
+		language, keyword, page, setting.UI.RepoSearchPagingNum, isMatch)
+	if err != nil {
+		if code_indexer.IsAvailable(ctx) {
+			ctx.ServerError("SearchResults", err)
 			return
 		}
-
-		ctx.Data["CodeIndexerDisabled"] = true
-		ctx.Data["SearchResults"] = data
-
-		pager := context.NewPagination(len(data), setting.UI.RepoSearchPagingNum, page, 5)
-		pager.SetDefaultParams(ctx)
-		ctx.Data["Page"] = pager
+		ctx.Data["CodeIndexerUnavailable"] = true
+	} else {
+		ctx.Data["CodeIndexerUnavailable"] = !code_indexer.IsAvailable(ctx)
 	}
+
+	ctx.Data["Repo"] = ctx.Repo.Repository
+	ctx.Data["SourcePath"] = ctx.Repo.Repository.Link()
+	ctx.Data["SearchResults"] = searchResults
+	ctx.Data["SearchResultLanguages"] = searchResultLanguages
+
+	pager := context.NewPagination(total, setting.UI.RepoSearchPagingNum, page, 5)
+	pager.SetDefaultParams(ctx)
+	pager.AddParam(ctx, "l", "Language")
+	ctx.Data["Page"] = pager
 
 	ctx.HTML(http.StatusOK, tplSearch)
 }
