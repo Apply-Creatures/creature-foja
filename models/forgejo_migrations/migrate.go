f1c1a1e877e5091ce52ea7f96c2b0a6bdfa7558b
Earl Warren <contact@earl-warren.org>
2024-04-11 11:13:28 +0200

    models: reserve v11 for backporting release blocker

--- models/forgejo_migrations/migrate.go
+++ models/forgejo_migrations/migrate.go
@@ -59,6 +59,8 @@ var migrations = []*Migration{
 	// v9 -> v10
 	NewMigration("Add pronouns to user", forgejo_v1_22.AddPronounsToUser),
 	// v11 -> v12
+	// NewMigration()
+	// v12 -> v13
 	NewMigration("Add repo_archive_download_count table", forgejo_v1_22.AddRepoArchiveDownloadCount),
 }
 
