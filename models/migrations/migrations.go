b9891088adb43f40a44050006317e098ff255727
wxiaoguang <wxiaoguang@gmail.com>
2024-04-17 23:58:37 +0800

    Allow everyone to read or write a wiki by a repo unit setting (#30495)
    Replace #6312
    Help #5833
    Wiki solution for #639
    
    (cherry picked from commit 3feba9f1f44156c256a30d25ad1c25f751819c94)
    
    Conflicts:
      Trash everything, just keep the migration placeholder to ensure the Gitea
      sequence is preserved. The Wiki edition is implemented differently.

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -584,6 +584,8 @@ var migrations = []Migration{
 	NewMigration("Add commit status summary table", v1_23.AddCommitStatusSummary),
 	// v296 -> v297
 	NewMigration("Add missing field of commit status summary table", v1_23.AddCommitStatusSummary2),
+	// v297 -> v298
+	NewMigration("Add everyone_access_mode for repo_unit", noopMigration),
 }
 
 // GetCurrentDBVersion returns the current db version
