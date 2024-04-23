168cb758ec70f0c44b603edfad0356ae82d60a9d
Zettat123 <zettat123@gmail.com>
2024-04-23 11:51:52 +0800

    Add a db consistency check to remove runners that do not belong to a repository (#30614)
    Follow #30406
    
    (cherry picked from commit 30dd4beeee631860c7dd393c341e9955997095a4)

--- services/doctor/dbconsistency.go
+++ services/doctor/dbconsistency.go
@@ -160,6 +160,12 @@ func checkDBConsistency(ctx context.Context, logger log.Logger, autofix bool) er
 			FixedMessage: "Removed",
 		},
 		{
+			Name:         "Action Runners without existing repository",
+			Counter:      actions_model.CountRunnersWithoutBelongingRepo,
+			Fixer:        actions_model.FixRunnersWithoutBelongingRepo,
+			FixedMessage: "Removed",
+		},
+		{
 			Name:         "Topics with empty repository count",
 			Counter:      repo_model.CountOrphanedTopics,
 			Fixer:        repo_model.DeleteOrphanedTopics,
