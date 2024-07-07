11ff38dd40177ecc151a2984fa7e124cf61ed657
Beowulf <beowulf@beocode.eu>
2024-07-07 19:59:32 +0200

    fix branch selector in commit graph
    Regression from https://codeberg.org/forgejo/forgejo/commit/5ef80ceb6437fa979d4ac567fa4b8c65c7e063d6

---
+++ tests/e2e/commit-graph-branch-selector.test.e2e.js
@@ -0,0 +1,25 @@
+// @ts-check
+import {test, expect} from '@playwright/test';
+import {login_user, load_logged_in_context} from './utils_e2e.js';
+
+test.beforeAll(async ({browser}, workerInfo) => {
+  await login_user(browser, workerInfo, 'user2');
+});
+
+test('Switch branch', async ({browser}, workerInfo) => {
+  const context = await load_logged_in_context(browser, workerInfo, 'user2');
+  const page = await context.newPage();
+  const response = await page.goto('/user2/repo1/graph');
+  await expect(response?.status()).toBe(200);
+
+  await page.click('#flow-select-refs-dropdown');
+  const input = page.locator('#flow-select-refs-dropdown');
+  await input.pressSequentially('develop', {delay: 50});
+  await input.press('Enter');
+
+  await page.waitForLoadState('networkidle');
+
+  await expect(page.locator('#loading-indicator')).not.toBeVisible();
+  await expect(page.locator('#rel-container')).toBeVisible();
+  await expect(page.locator('#rev-container')).toBeVisible();
+});
