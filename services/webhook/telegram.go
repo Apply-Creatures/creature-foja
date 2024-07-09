38dce65dc1e5604f93f86eb5858c08e403067ca3
wxiaoguang <wxiaoguang@gmail.com>
2024-07-09 11:23:33 +0800

    Use old behavior for telegram webhook (#31588)
    Fix #31182
    
    (cherry picked from commit 43c63c33aea1b0725ed1fe7b4a55dd85050f0beb)

--- services/webhook/telegram.go
+++ services/webhook/telegram.go
@@ -15,6 +15,7 @@ import (
 	"code.gitea.io/gitea/modules/git"
 	"code.gitea.io/gitea/modules/json"
 	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/markup"
 	api "code.gitea.io/gitea/modules/structs"
 	webhook_module "code.gitea.io/gitea/modules/webhook"
 	"code.gitea.io/gitea/services/forms"
@@ -213,7 +214,7 @@ func (t telegramConvertor) Package(p *api.PackagePayload) (TelegramPayload, erro
 
 func createTelegramPayload(message string) TelegramPayload {
 	return TelegramPayload{
-		Message:           strings.TrimSpace(message),
+		Message:           markup.Sanitize(strings.TrimSpace(message)),
 		ParseMode:         "HTML",
 		DisableWebPreview: true,
 	}
