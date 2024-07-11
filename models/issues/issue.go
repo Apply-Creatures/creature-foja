9f461e180a8ee51bfe17c1bf6a9df03d68b7856d
Robert Wolff <mahlzahn@posteo.de>
2024-07-11 18:54:44 +0200

    remove requirement on trailing character after check box

--- models/issues/issue.go
+++ models/issues/issue.go
@@ -153,8 +153,8 @@ type Issue struct {
 }
 
 var (
-	issueTasksPat     = regexp.MustCompile(`(^\s*[-*]\s*\[[\sxX]\].)|(\n\s*[-*]\s*\[[\sxX]\].)`)
-	issueTasksDonePat = regexp.MustCompile(`(^\s*[-*]\s*\[[xX]\].)|(\n\s*[-*]\s*\[[xX]\].)`)
+	issueTasksPat     = regexp.MustCompile(`(^\s*[-*]\s*\[[\sxX]\])|(\n\s*[-*]\s*\[[\sxX]\])`)
+	issueTasksDonePat = regexp.MustCompile(`(^\s*[-*]\s*\[[xX]\])|(\n\s*[-*]\s*\[[xX]\])`)
 )
 
 // IssueIndex represents the issue index table
