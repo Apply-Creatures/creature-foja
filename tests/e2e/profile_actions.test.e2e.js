14d9c386fd05cbd0dd37253806137a09c3dec324
Gusted <postmaster@gusted.xyz>
2024-07-16 21:30:47 +0200

    [UI] Fix HTMX support for profile card
    - There were two issues with the profile card since the introduction of
    HTMX in 3e8414179c3f3e8a12d3a66fdf32c144f941f5c3. If an HTMX request
    resulted in a flash message, it wasn't being shown and HTMX was
    replacing all the HTML content instead of morphing it into the existing
    DOM which caused event listeners to be lost for buttons.
    - Flash messages are now properly being shown by using `hx-swap-oob`
    and sending the alerts on a HTMX request, this does mean it requires
    server-side changes in order to support HTMX requests like this, but
    it's luckily not a big change either.
    - Morphing is now enabled for the profile card by setting
    `hx-swap="morph"`, and weirdly, the morphing library was already
    installed and included as a dependency. This solves the issue of buttons
    losing their event listeners.
    - This patch also adds HTMX support to the modals feature, which means
    that the blocking feature on the profile card now takes advantage of
    HTMX.
    - Added a E2E test.

---
+++ tests/e2e/profile_actions.test.e2e.js
@@ -0,0 +1,41 @@
+// @ts-check
+import {test, expect} from '@playwright/test';
+import {login_user, load_logged_in_context} from './utils_e2e.js';
+
+test('Follow actions', async ({browser}, workerInfo) => {
+  await login_user(browser, workerInfo, 'user2');
+  const context = await load_logged_in_context(browser, workerInfo, 'user2');
+  const page = await context.newPage();
+
+  await page.goto('/user1');
+  await page.waitForLoadState('networkidle');
+
+  // Check if following and then unfollowing works.
+  // This checks that the event listeners of
+  // the buttons aren't dissapearing.
+  const followButton = page.locator('.follow');
+  await expect(followButton).toContainText('Follow');
+  await followButton.click();
+  await expect(followButton).toContainText('Unfollow');
+  await followButton.click();
+  await expect(followButton).toContainText('Follow');
+
+  // Simple block interaction.
+  await expect(page.locator('.block')).toContainText('Block');
+
+  await page.locator('.block').click();
+  await expect(page.locator('#block-user')).toBeVisible();
+  await page.locator('#block-user .ok').click();
+  await expect(page.locator('.block')).toContainText('Unblock');
+  await expect(page.locator('#block-user')).not.toBeVisible();
+
+  // Check that following the user yields in a error being shown.
+  await followButton.click();
+  const flashMessage = page.locator('#flash-message');
+  await expect(flashMessage).toBeVisible();
+  await expect(flashMessage).toContainText('You cannot follow this user because you have blocked this user or this user has blocked you.');
+
+  // Unblock interaction.
+  await page.locator('.block').click();
+  await expect(page.locator('.block')).toContainText('Block');
+});
